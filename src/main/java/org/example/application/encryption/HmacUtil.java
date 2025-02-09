88e0eb89267f326ab7a84aba6d31963a8332bf55
jaredgabriel2001 <gabrieljaredjared@gmail.com>
2025-02-08 21:27:08 -0300

    cheio de cagada

--- src/main/java/org/example/application/encryption/HmacUtil.java
+++ src/main/java/org/example/application/encryption/HmacUtil.java
@@ -1,8 +1,4 @@
-<<<<<<<< HEAD:src/main/java/org/example/application/encryption/HmacUtil.java
 package org.example.application.encryption;
-========
-package org.encryption;
->>>>>>>> 0c91afaf11321c3615c5e84241311176c786fa2c:src/main/java/org/encryption/HmacUtil.java
 
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
